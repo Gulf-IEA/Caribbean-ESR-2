d41104140236e8cd94a44b6d2da5834e5c468046
CarissaGervasi-NOAA <carissa.gervasi@noaa.gov>
2025-05-08 14:16:55 -0400

    completing a test tech doc

--- Tech_Doc/testing.R
+++ Tech_Doc/testing.R
@@ -1,8 +1,11 @@
 issue <- pull_single_issue(9)
 parsed <- parse_issue(issue$body)
-str(parsed, max.level = 1)
-
-
-parsed <- parse_issue(issue$body)
 clean_df <- create_object(parsed)
 write_qmd(clean_df, output_path = "Tech_Doc/Test_data.qmd")
+
+
+
+issue2 <- pull_single_issue(10)
+parsed2 <- parse_issue(issue2$body)
+clean_df2 <- create_object(parsed2)
+write_qmd(clean_df2, output_path = "Tech_Doc/Test_data_2.qmd")
