49c535bf9d5e3b4b11f5ad8f68d1d998ff8f0568
CarissaGervasi-NOAA <carissa.gervasi@noaa.gov>
2025-05-09 12:28:34 -0400

    trying to fix tech doc

--- Tech_Doc/testing.R
+++ Tech_Doc/testing.R
@@ -1,3 +1,6 @@
+
+library(fs)
+
 issue <- pull_single_issue(9)
 parsed <- parse_issue(issue$body)
 clean_df <- create_object(parsed)
@@ -9,3 +12,11 @@ issue2 <- pull_single_issue(10)
 parsed2 <- parse_issue(issue2$body)
 clean_df2 <- create_object(parsed2)
 write_qmd(clean_df2, output_path = "Tech_Doc/Test_data_2.qmd")
+
+
+# Render Tech_Doc in place
+quarto::quarto_render("Tech_Doc/index.qmd")
+
+# Then copy the output to _book/tech-doc
+fs::dir_copy("Tech_Doc/_site", "_book/tech-doc", overwrite = TRUE)
+
